b16de8b4a33bf74f5307449ddd7484bca4e4bee8
Adanos020 <adanos020@gmail.com>
2024-11-21 15:59:13 +0000

    Initial commit

---
+++ Source/FunWithCubes/FunWithCubes.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FunWithCubes.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FunWithCubes, "FunWithCubes" );
